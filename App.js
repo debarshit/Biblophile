2ba70d8aa81332fb00fa4e3c347e836ce82e4ab6
debarshi <dune02deb@gmail.com>
2022-09-29 17:52:36 +0530

    Added styled components

--- App.js
+++ App.js
@@ -1,36 +1,14 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 
+import { HomeScreen } from "./src/features/home/screens/home.screen";
 
 export default function App() {
   return (
     <>
-    <SafeAreaView style={styles.container}>
-      <View style={styles.search}>
-        <Searchbar />
-      </View>
-      <View style={styles.list}>
-        <Text>list</Text>
-      </View>
-    </SafeAreaView>
+    <HomeScreen />
     <ExpoStatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
