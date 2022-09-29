f69beb6663b3e90dd6333aeb03f484cb1bea84a3
debarshi <dune02deb@gmail.com>
2022-09-29 23:48:41 +0530

    Basic theming done

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
+import { ThemeProvider } from "styled-components/native";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
 import { HomeScreen } from "./src/features/home/screens/home.screen";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+  
   return (
     <>
-    <HomeScreen />
+    <ThemeProvider theme={theme}>
+      <HomeScreen />
+    </ThemeProvider>
     <ExpoStatusBar style="auto" />
     </>
   );
--- src/features/home/screens/home.screen.js
+++ src/features/home/screens/home.screen.js
@@ -9,13 +9,12 @@ const SafeArea = styled(SafeAreaView)`
 `;
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const BookGenreListContainer = styled.View`
   flex: 1;
-  padding: 16px;
-  background-color: blue;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 export const HomeScreen = () => (
